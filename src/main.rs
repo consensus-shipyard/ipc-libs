5502c29e34b57ae4d7a2ae0d1de3f58b47cc9c52
cryptoAtwill <willes.lau@protocol.ai>
2023-01-26 13:56:04 +0800

    add comments to json rpc main

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ use crate::jsonrpc::JsonRpcClientImpl;
 mod jsonrpc;
 
 fn main() {
+    // just some dummy code to silent unused code warning, change this in actual build
     JsonRpcClientImpl::new("".parse().unwrap(), None);
 }
