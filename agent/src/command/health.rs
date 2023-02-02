5154128f06d973826beb7c0ac7e62600bd572e7f
cryptoAtwill <108330426+cryptoAtwill@users.noreply.github.com>
2023-02-02 11:26:30 +0800

    Cleanup (#3)
    * clean up
    
    * add readme
    
    * fix clippy

--- agent/src/command/health.rs
+++ agent/src/command/health.rs
@@ -4,14 +4,13 @@ use lazy_static::lazy_static;
 use serde::{Deserialize, Serialize};
 
 use crate::common::config::{DEFAULT_NODE_ADDR, DEFAULT_PROTOCOL, DEFAULT_RPC_ENDPOINT};
+use crate::common::error::Error;
 use crate::common::handlers::{CommandLineHandler, RPCNodeHandler};
 use crate::common::rpc::{JSONRPCParam, JSONRPCResponse};
 
 lazy_static! {
-    static ref DEFAULT_URL: String = format!(
-        "{}://{}/{}",
-        DEFAULT_PROTOCOL, DEFAULT_NODE_ADDR, DEFAULT_RPC_ENDPOINT
-    );
+    static ref DEFAULT_URL: String =
+        format!("{DEFAULT_PROTOCOL}://{DEFAULT_NODE_ADDR}/{DEFAULT_RPC_ENDPOINT}");
 }
 
 #[derive(Debug, Args)]
@@ -36,16 +35,14 @@ pub struct HealthCheckCmd {}
 #[async_trait]
 impl CommandLineHandler for HealthCheckCmd {
     type Request = HealthCheck;
-    type Error = ();
 
-    async fn handle(request: &Self::Request) -> Result<(), Self::Error> {
+    async fn handle(request: &Self::Request) -> Result<String, Error> {
         let node = request.node_endpoint.as_ref().unwrap_or(&DEFAULT_URL);
         if is_health(node).await {
-            println!("node: {:} is healthy", node);
+            Ok(format!("node: {node:} is healthy"))
         } else {
-            println!("node: {:} is not healthy", node);
+            Err(Error::Custom(format!("node: {node:} is down")))
         }
-        Ok(())
     }
 }
 
@@ -61,17 +58,16 @@ impl RPCNodeHandler for HealthCheckCmd {
 }
 
 async fn is_health(node: &str) -> bool {
-    log::debug!("health check endpoint: {:}", node);
+    log::debug!("health check endpoint: {node:}");
 
     let client = reqwest::Client::new();
     match client
         .post(node)
-        .json(&JSONRPCParam {
-            id: 0,
-            jsonrpc: "2.0".to_string(),
-            method: "health_check".to_string(),
-            params: serde_json::Value::Null,
-        })
+        .json(&JSONRPCParam::new(
+            0,
+            "health_check".to_string(),
+            serde_json::Value::Null,
+        ))
         .send()
         .await
     {
--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,17 +1,23 @@
-use agent::command::health::HealthCheckCmd;
-use agent::{register_cli_command, register_server_routes};
+mod command;
+mod common;
+
+use crate::command::health::HealthCheckCmd;
+use crate::command::node::NodeCmd;
 
 // define the routes here
 register_server_routes!(
     // initialize your server RPC handlers here, returns the handlers as a tuple
     init: {
-        use agent::command::health::HealthCheckCmd;
+        use crate::command::health::HealthCheckCmd;
 
         let h1 = HealthCheckCmd {};
         let h2 = HealthCheckCmd {};
 
         (h1, h2)
     },
+    // health_check method is associated with h1
+    // health_check2 method is associated with h2
+    // TODO: find a clearer way to association and not by convention
     commands: health_check, health_check2
 );
 
@@ -19,11 +25,13 @@ register_server_routes!(
 register_cli_command!(
     // { COMMAND NAME, HANDLER }
     {HealthCheck, HealthCheckCmd},
-    {Node, node::NodeCmd}
+    {Node, NodeCmd}
 );
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    env_logger::init_from_env(
+        env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"),
+    );
     cli().await;
 }
