0e8b0ac5e019d11de18d3e8a09787f39160edcea
cryptoAtwill <willes.lau@protocol.ai>
2023-02-21 21:58:36 +0800

    resolve with main

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -43,6 +43,7 @@ impl Config {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
     use std::net::SocketAddr;
     use std::str::FromStr;
 
@@ -51,72 +52,88 @@ mod tests {
     use ipc_sdk::subnet_id::{ROOTNET_ID, SubnetID};
     use url::Url;
 
-    use crate::config::Config;
+    use crate::config::{Config, Server, Subnet};
+
+    // Arguments for the config's fields
+    const SERVER_JSON_RPC_ADDR: &str = "127.0.0.1:3030";
+    const ROOT_ID: &str = "/root";
+    const CHILD_ID: &str = "/root/f0100";
+    const ROOT_AUTH_TOKEN: &str = "ROOT_AUTH_TOKEN";
+    const CHILD_AUTH_TOKEN: &str = "CHILD_AUTH_TOKEN";
+    const JSONRPC_API_HTTP: &str = "https://example.org/rpc/v0";
+    const JSONRPC_API_WS: &str = "ws://example.org/rpc/v0";
+    const ACCOUNT_ADDRESS: &str = "f3thgjtvoi65yzdcoifgqh6utjbaod3ukidxrx34heu34d6avx6z7r5766t5jqt42a44ehzcnw3u5ehz47n42a";
 
     #[test]
-    fn read_config() {
-        // Arguments for the config's fields
-        let server_json_rpc_addr = "127.0.0.1:3030";
+    fn check_server_config() {
+        let config = read_config().server;
+        assert_eq!(
+            config.json_rpc_address,
+            SocketAddr::from_str(SERVER_JSON_RPC_ADDR).unwrap(),
+            "invalid server rpc address"
+        );
+    }
 
-        let root_id = "/root";
-        let child_id = "/root/f0100";
-        let root_auth_token = "root_auth_token";
-        let child_auth_token = "child_auth_token";
-        let jsonrpc_api_http = "https://example.org/rpc/v0";
-        let jsonrpc_api_ws = "ws://example.org/rpc/v0";
-        let account_address = "f3thgjtvoi65yzdcoifgqh6utjbaod3ukidxrx34heu34d6avx6z7r5766t5jqt42a44ehzcnw3u5ehz47n42a";
+    #[test]
+    fn check_subnets_config() {
+        let config = read_config().subnets;
 
+        let root = &config["root"];
+        assert_eq!(root.id, *ROOTNET_ID);
+        assert_eq!(
+            root.jsonrpc_api_http,
+            Url::from_str(JSONRPC_API_HTTP).unwrap()
+        );
+        assert_eq!(
+            root.jsonrpc_api_ws.as_ref().unwrap(),
+            &Url::from_str(JSONRPC_API_WS).unwrap()
+        );
+        assert_eq!(
+            root.auth_token.as_ref().unwrap(),
+            ROOT_AUTH_TOKEN
+        );
+
+        let child = &config["child"];
+        assert_eq!(
+            child.id,
+            SubnetID::from_str(CHILD_ID).unwrap(),
+        );
+        assert_eq!(
+            child.jsonrpc_api_http,
+            Url::from_str(JSONRPC_API_HTTP).unwrap(),
+        );
+        assert_eq!(
+            child.auth_token.as_ref().unwrap(),
+            CHILD_AUTH_TOKEN,
+        );
+        assert_eq!(
+            child.accounts.as_ref(),
+            vec![Address::from_str(ACCOUNT_ADDRESS).unwrap()],
+        );
+    }
+
+    fn read_config() -> Config {
         let config_str = formatdoc!(
             r#"
             [server]
-            json_rpc_address = "{server_json_rpc_addr}"
+            json_rpc_address = "{SERVER_JSON_RPC_ADDR}"
 
             [subnets]
 
             [subnets.root]
-            id = "{root_id}"
-            jsonrpc_api_http = "{jsonrpc_api_http}"
-            jsonrpc_api_ws = "{jsonrpc_api_ws}"
-            auth_token = "{root_auth_token}"
+            id = "{ROOT_ID}"
+            jsonrpc_api_http = "{JSONRPC_API_HTTP}"
+            jsonrpc_api_ws = "{JSONRPC_API_WS}"
+            auth_token = "{ROOT_AUTH_TOKEN}"
 
             [subnets.child]
-            id = "{child_id}"
-            jsonrpc_api_http = "{jsonrpc_api_http}"
-            auth_token = "{child_auth_token}"
-            accounts = ["{account_address}"]
+            id = "{CHILD_ID}"
+            jsonrpc_api_http = "{JSONRPC_API_HTTP}"
+            auth_token = "{CHILD_AUTH_TOKEN}"
+            accounts = ["{ACCOUNT_ADDRESS}"]
         "#
         );
 
-        println!("{}", config_str);
-        let config = Config::from_str(config_str.as_str()).unwrap();
-
-        assert_eq!(
-            config.server.json_rpc_address,
-            SocketAddr::from_str(server_json_rpc_addr).unwrap()
-        );
-
-        let root = &config.subnets["root"];
-        assert_eq!(root.id, *ROOTNET_ID);
-        assert_eq!(
-            root.jsonrpc_api_http,
-            Url::from_str(jsonrpc_api_http).unwrap()
-        );
-        assert_eq!(
-            root.jsonrpc_api_ws.as_ref().unwrap(),
-            &Url::from_str(jsonrpc_api_ws).unwrap()
-        );
-        assert_eq!(root.auth_token.as_ref().unwrap(), root_auth_token);
-
-        let child = &config.subnets["child"];
-        assert_eq!(child.id, SubnetID::from_str(child_id).unwrap());
-        assert_eq!(
-            child.jsonrpc_api_http,
-            Url::from_str(jsonrpc_api_http).unwrap()
-        );
-        assert_eq!(child.auth_token.as_ref().unwrap(), child_auth_token);
-        assert_eq!(
-            child.accounts.as_ref(),
-            vec![Address::from_str(account_address).unwrap()]
-        );
+        Config::from_str(config_str.as_str()).unwrap()
     }
 }
