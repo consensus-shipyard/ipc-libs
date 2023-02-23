0d3322c71ecb3d320fc8ade80544fe89d5f51c5d
cryptoAtwill <willes.lau@protocol.ai>
2023-02-23 15:56:50 +0800

    update local lotus url

--- tests/setup/mod.rs
+++ tests/setup/mod.rs
@@ -1,7 +1,7 @@
 use ipc_client::jsonrpc::JsonRpcClientImpl;
 use ipc_client::lotus::LotusJsonRPCClient;
 
-pub const LOCAL_JSON_RPC_HTTP_URL: &str = "https://api.node.glif.io/rpc/v0";
+pub const LOCAL_JSON_RPC_HTTP_URL: &str = "http://localhost:3030/rpc/v0";
 
 pub fn http_json_rpc(url: &str, bearer_token: Option<&str>) -> JsonRpcClientImpl {
     JsonRpcClientImpl::new(url.parse().unwrap(), bearer_token)
