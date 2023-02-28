6fae6de79d47345c5957c1679f63f25f91d3f0e9
cryptoAtwill <willes.lau@protocol.ai>
2023-02-28 09:56:04 +0800

    add json rpc handler trait

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,5 +1,20 @@
 //! The ipc-agent json rpc node.
 
+use std::fmt::Debug;
+use anyhow::Result;
+use serde::de::DeserializeOwned;
+use async_trait::async_trait;
+
 pub mod jsonrpc;
 pub mod request;
 pub mod response;
+
+/// The JSON RPC server request handler trait.
+#[async_trait]
+pub trait JsonRPCRequestHandler {
+    type Request: Debug;
+    type Response: Debug + DeserializeOwned;
+
+    /// Handles the request sent to the json rpc server. Returns a response back.
+    async fn handle(&self, request: &Self::Request) -> Result<Self::Response>;
+}
