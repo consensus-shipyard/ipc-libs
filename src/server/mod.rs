6f3cf5670572f0281b3eedeeceadf89be3096165
cryptoAtwill <willes.lau@protocol.ai>
2023-02-28 10:05:57 +0800

    update response to serialize

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -2,7 +2,7 @@
 
 use anyhow::Result;
 use async_trait::async_trait;
-use serde::de::DeserializeOwned;
+use serde::Serialize;
 use std::fmt::Debug;
 
 pub mod jsonrpc;
@@ -13,7 +13,7 @@ pub mod response;
 #[async_trait]
 pub trait JsonRPCRequestHandler {
     type Request: Debug;
-    type Response: Debug + DeserializeOwned;
+    type Response: Debug + Serialize;
 
     /// Handles the request sent to the json rpc server. Returns a response back.
     async fn handle(&self, request: &Self::Request) -> Result<Self::Response>;
