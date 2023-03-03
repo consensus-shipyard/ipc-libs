ceb70ca5a087a91194d6b4772119940f17e5d758
Henrique Moniz <1785239+hmoniz@users.noreply.github.com>
2023-03-03 10:53:52 +0000

    Adds JSON-RPC method calls to read the state of the IPC actors and a convenience method to construct a LotusJsonRPCClient from a subnet config.

--- src/lotus/message/ipc.rs
+++ src/lotus/message/ipc.rs
@@ -1,5 +1,7 @@
 // Copyright 2022-2023 Protocol Labs
 // SPDX-License-Identifier: MIT
+use fvm_shared::clock::ChainEpoch;
+use ipc_subnet_actor::Validator;
 use serde::Deserialize;
 
 use crate::lotus::message::CIDMap;
@@ -9,4 +11,17 @@ use crate::lotus::message::CIDMap;
 pub struct IPCGetPrevCheckpointForChildResponse {
     #[serde(rename = "CID")]
     pub cid: CIDMap,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct IPCReadGatewayStateResponse {
+    pub check_period: ChainEpoch,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct IPCReadSubnetActorStateResponse {
+    pub check_period: ChainEpoch,
+    pub validator_set: Vec<Validator>,
 }
