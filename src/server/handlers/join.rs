319c22758eb9d0bc056e814e1ee6e775519eac21
cryptoAtwill <willes.lau@protocol.ai>
2023-03-07 19:09:06 +0800

    add more methods

---
+++ src/server/handlers/join.rs
@@ -0,0 +1,62 @@
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: MIT
+//! Create subnet handler and parameters
+
+use crate::manager::SubnetManager;
+use crate::server::handlers::subnet::SubnetManagerPool;
+use crate::server::JsonRPCRequestHandler;
+use anyhow::anyhow;
+use async_trait::async_trait;
+use fvm_shared::address::Address;
+use fvm_shared::econ::TokenAmount;
+use ipc_sdk::subnet_id::SubnetID;
+use ipc_subnet_actor::JoinParams;
+use serde::{Deserialize, Serialize};
+use std::str::FromStr;
+use std::sync::Arc;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct JoinSubnetParams {
+    pub subnet: String,
+    pub from: Option<String>,
+    pub collateral: u64,
+    pub validator_net_addr: String,
+}
+
+/// The create subnet json rpc method handler.
+pub(crate) struct JoinSubnetHandler {
+    pool: Arc<SubnetManagerPool>,
+}
+
+impl JoinSubnetHandler {
+    pub(crate) fn new(pool: Arc<SubnetManagerPool>) -> Self {
+        Self { pool }
+    }
+}
+
+#[async_trait]
+impl JsonRPCRequestHandler for JoinSubnetHandler {
+    type Request = JoinSubnetParams;
+    type Response = ();
+
+    async fn handle(&self, request: Self::Request) -> anyhow::Result<Self::Response> {
+        let conn = match self.pool.get(&request.subnet) {
+            None => return Err(anyhow!("target parent subnet not found")),
+            Some(conn) => conn,
+        };
+
+        let join_params = JoinParams {
+            validator_net_addr: request.validator_net_addr,
+        };
+        let subnet = SubnetID::from_str(&request.subnet)?;
+        let collateral = TokenAmount::from_atto(request.collateral);
+        let from = match request.from {
+            Some(addr) => Address::from_str(&addr)?,
+            None => conn.subnet().accounts[0],
+        };
+
+        conn.manager()
+            .join_subnet(subnet, from, collateral, join_params)
+            .await
+    }
+}
--- src/server/handlers/mod.rs
+++ src/server/handlers/mod.rs
@@ -4,12 +4,18 @@
 
 mod config;
 pub mod create;
+mod join;
+mod kill;
+mod leave;
 mod subnet;
 
 use crate::config::json_rpc_methods;
 use crate::config::ReloadableConfig;
 use crate::server::create::CreateSubnetHandler;
 use crate::server::handlers::config::ReloadConfigHandler;
+use crate::server::handlers::join::JoinSubnetHandler;
+use crate::server::handlers::kill::KillSubnetHandler;
+use crate::server::handlers::leave::LeaveSubnetHandler;
 use crate::server::handlers::subnet::SubnetManagerPool;
 use crate::server::JsonRPCRequestHandler;
 use anyhow::{anyhow, Result};
@@ -18,18 +24,59 @@ use serde_json::Value;
 use std::collections::HashMap;
 use std::sync::Arc;
 
-pub type Method = String;
+/// A util macro to create the handlers and do the method routing.
+/// This macro generates the `handle` method. In the method, it just matches the handler with
+/// different enum types to get the inner handler instance. Every time a new handler is added,
+/// a new match statement must be copy-pasted. With this macro, we can reduce a lot of repetitive
+/// copy paste.
+macro_rules! create_handlers {
+    (enum HandlerWrapper { $($name:tt($handler:tt),)* }) => {
+        /// A util enum to avoid Box<dyn> mess in Handlers struct
+        enum HandlerWrapper {
+            $(
+            $name($handler)
+            ),*
+        }
 
-/// A util enum to avoid Box<dyn> mess in Handlers struct
-enum HandlerWrapper {
-    CreateSubnet(CreateSubnetHandler),
-    ReloadConfig(ReloadConfigHandler),
+        impl Handlers {
+            pub async fn handle(&self, method: Method, params: Value) -> Result<Value> {
+                if let Some(wrapper) = self.handlers.get(&method) {
+                    match wrapper {
+                        $(
+                            HandlerWrapper::$name(handler) => {
+                                let r = handler.handle(serde_json::from_value(params)?).await?;
+                                Ok(serde_json::to_value(r)?)
+                            }
+                        ),*
+                    }
+                } else {
+                    Err(anyhow!("method not supported"))
+                }
+            }
+        }
+    };
+    (enum HandlerWrapper { $($name:tt($handler:tt)),* }) => {
+        create_handlers!(enum HandlerWrapper { $($name($handler),)* });
+    }
 }
+
+pub type Method = String;
 
 /// The collection of all json rpc handlers
 pub struct Handlers {
     handlers: HashMap<Method, HandlerWrapper>,
 }
+
+// Create the handler wrapper
+create_handlers!(
+    enum HandlerWrapper {
+        CreateSubnet(CreateSubnetHandler),
+        JoinSubnet(JoinSubnetHandler),
+        LeaveSubnet(LeaveSubnetHandler),
+        KillSubnet(KillSubnetHandler),
+        ReloadConfig(ReloadConfigHandler),
+    }
+);
 
 impl Handlers {
     /// We test the handlers separately and individually instead of from the handlers.
@@ -53,29 +100,25 @@ impl Handlers {
             )),
         );
 
+        // subnet manager methods
         let pool = Arc::new(SubnetManagerPool::from_reload_config(config));
         handlers.insert(
             String::from(json_rpc_methods::CREATE_SUBNET),
-            HandlerWrapper::CreateSubnet(CreateSubnetHandler::new(pool)),
+            HandlerWrapper::CreateSubnet(CreateSubnetHandler::new(pool.clone())),
+        );
+        handlers.insert(
+            String::from(json_rpc_methods::CREATE_SUBNET),
+            HandlerWrapper::LeaveSubnet(LeaveSubnetHandler::new(pool.clone())),
+        );
+        handlers.insert(
+            String::from(json_rpc_methods::CREATE_SUBNET),
+            HandlerWrapper::KillSubnet(KillSubnetHandler::new(pool.clone())),
+        );
+        handlers.insert(
+            String::from(json_rpc_methods::JOIN_SUBNET),
+            HandlerWrapper::JoinSubnet(JoinSubnetHandler::new(pool)),
         );
 
         Ok(Self { handlers })
-    }
-
-    pub async fn handle(&self, method: Method, params: Value) -> Result<Value> {
-        if let Some(wrapper) = self.handlers.get(&method) {
-            match wrapper {
-                HandlerWrapper::CreateSubnet(handler) => {
-                    let r = handler.handle(serde_json::from_value(params)?).await?;
-                    Ok(serde_json::to_value(r)?)
-                }
-                HandlerWrapper::ReloadConfig(handler) => {
-                    handler.handle(serde_json::from_value(params)?).await?;
-                    Ok(serde_json::to_value(())?)
-                }
-            }
-        } else {
-            Err(anyhow!("method not supported"))
-        }
     }
 }
