508fa6f5674c9bb8846ea64fcfc58e856331881d
cryptoAtwill <willes.lau@protocol.ai>
2023-03-09 15:25:20 +0800

    update comment

--- src/jsonrpc/mod.rs
+++ src/jsonrpc/mod.rs
@@ -127,9 +127,9 @@ impl<T: DeserializeOwned> From<JsonRpcResponse<T>> for Result<T> {
         if j.result.is_some() {
             Ok(j.result.unwrap())
         } else {
-            // the result is not found, but it is possible T could (),  means the
-            // caller does not expect any response. Need to rely on serde to perform
-            // a conversion.
+            // the result is not found, but it is possible T could the rust unit type: (), i.e. the
+            // caller is expecting Result<()>.
+            // To do this, we need to rely on serde to perform a conversion from NULL.
             Ok(serde_json::from_value(serde_json::Value::Null)?)
         }
     }
