8e989bd571350b5a3ab72792b0b6ff4a84cc12d9
Henrique Moniz <1785239+hmoniz@users.noreply.github.com>
2023-03-13 09:56:11 +0000

    Adds JSON-RPC method calls to read the state of the IPC actors and a convenience method to construct a LotusJsonRPCClient from a subnet config.
    fix merge conflicts
    
    Fix formatting
    
    allow dead_code in from_subnet()
    
    Implements logic for actively checkpointing the state of managed subnets.
    
    Remove spurious println
    
    Remove commented code
    
    Fix formatting
    
    Add license header
    
    Fix clippy errors
    
    allow dead_code in start()
    
    remove allow_deadcode
    
    Add TODO for one-block tipset assumption
    
    Add hot reloading to checkpoint logic
    
    Fix
    
    unfix
    
    shutdown subsystem
    
    clean up
    
    .
    
    more fixes
    
    address nits
    
    fix merge conflicts
    
    Adds JSON-RPC method calls to read the state of the IPC actors and a convenience method to construct a LotusJsonRPCClient from a subnet config.
    
    fix merge conflicts
    
    Fix formatting
    
    allow dead_code in from_subnet()
    
    Implements logic for actively checkpointing the state of managed subnets.
    
    Remove spurious println
    
    Remove commented code
    
    Fix formatting
    
    Add license header
    
    Fix clippy errors
    
    allow dead_code in start()
    
    remove allow_deadcode
    
    Add TODO for one-block tipset assumption
    
    Add hot reloading to checkpoint logic
    
    Fix
    
    unfix
    
    shutdown subsystem
    
    clean up
    
    .
    
    more fixes
    
    address nits
    
    fix merge conflicts

--- src/manager/mod.rs
+++ src/manager/mod.rs
@@ -1,7 +1,8 @@
 // Copyright 2022-2023 Protocol Labs
 // SPDX-License-Identifier: MIT
-mod lotus;
-mod subnet;
-
 pub use lotus::LotusSubnetManager;
 pub use subnet::SubnetManager;
+
+pub(crate) mod checkpoint;
+mod lotus;
+mod subnet;
