579ee660b3a09d79a7fb127a70b1d0d4983647f5
Alfonso de la Rocha <adlrocha@tutamail.com>
2023-03-17 18:48:43 +0100

    increase default timeout

--- src/jsonrpc/mod.rs
+++ src/jsonrpc/mod.rs
@@ -25,7 +25,7 @@ mod tests;
 
 const DEFAULT_JSON_RPC_VERSION: &str = "2.0";
 const DEFAULT_JSON_RPC_ID: u8 = 1;
-const DEFAULT_REQ_TIMEOUT: Duration = Duration::from_secs(10);
+const DEFAULT_REQ_TIMEOUT: Duration = Duration::from_secs(30);
 
 /// A convenience constant that represents empty params in a JSON-RPC request.
 pub const NO_PARAMS: Value = json!([]);
