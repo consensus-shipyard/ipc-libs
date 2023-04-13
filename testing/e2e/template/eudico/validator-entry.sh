f158ad64118ee9a21903e2e010d0043e4843d623
Akosh Farkash <aakoshh@gmail.com>
2023-04-13 18:55:04 +0100

    IPC-91: Fix API address for the validator

--- testing/e2e/template/eudico/validator-entry.sh
+++ testing/e2e/template/eudico/validator-entry.sh
@@ -2,8 +2,22 @@
 
 set -e
 
-if [ "${IPC_SUBNET_ID}" == "/root"]; then
+# By this time the daemon should have written to the LOTUS_PATH
+# the API token we can use to contact the server.
+while [ ! -f $LOTUS_PATH/token ]; do
+  echo "Waiting for the API token to appear...";
+  sleep 5
+done
+
+API_TOKEN=$(cat $LOTUS_PATH/token)
+
+# Set the env var that Lotus is looking for.
+export FULLNODE_API_INFO=${API_TOKEN}:/dns/${DAEMON_HOSTNAME}/tcp/1234/http
+
+if [ "${IPC_SUBNET_ID}" == "/root" ]; then
+  echo "Running as root net..."
   exec /scripts/ipc/src/root-single-validator.sh
 else
+  echo "Running as subnet..."
   exec /scripts/ipc/src/subnet-validator.sh
 fi
