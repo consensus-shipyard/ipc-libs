b086f1a2cf0f24b83278306b405e5c8104eec6eb
Akosh Farkash <aakoshh@gmail.com>
2023-04-18 14:02:29 +0100

    IPC-91: Simpler subnet to demonstrate the error

---
+++ testing/e2e/topology/simple.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Topology compiled from topology/simple.yaml
+set -e
+# Create the agent(s)
+make agent/up IPC_AGENT_NR=0
+# Create the root node(s)
+make node/up IPC_NODE_NR=0 IPC_SUBNET_NAME=head
+# Alternate connecting agents and creating subnets and nodes to run them
+make connect IPC_AGENT_NR=0 IPC_NODE_NR=0
+make node/up IPC_AGENT_NR=0 IPC_NODE_NR=1 IPC_PARENT_NR=0 IPC_WALLET_NR=0 IPC_SUBNET_NAME=thorax
+make connect IPC_AGENT_NR=0 IPC_NODE_NR=1
+make node/up IPC_AGENT_NR=0 IPC_NODE_NR=2 IPC_PARENT_NR=1 IPC_WALLET_NR=0 IPC_SUBNET_NAME=abdomen
+make connect IPC_AGENT_NR=0 IPC_NODE_NR=2
