3b4011ac0779cebe503d81e59204b3ee3050bad3
Alfonso de la Rocha <adlrocha@tutamail.com>
2023-05-04 19:46:41 +0200

    add license

--- src/keystore/keystore.rs
+++ src/keystore/keystore.rs
@@ -1,3 +1,5 @@
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: MIT
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
--- src/keystore/mod.rs
+++ src/keystore/mod.rs
@@ -1,3 +1,5 @@
+// Copyright 2022-2023 Protocol Labs
+// SPDX-License-Identifier: MIT
 mod errors;
 mod keystore;
 mod utils;
