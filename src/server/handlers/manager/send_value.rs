59001b827becb6790612d40bbf84f87769c4b6d3
cryptoAtwill <willes.lau@protocol.ai>
2023-05-08 15:56:20 +0800

    convert all amount to f64

--- src/server/handlers/manager/send_value.rs
+++ src/server/handlers/manager/send_value.rs
@@ -5,11 +5,10 @@
 use crate::manager::SubnetManager;
 use crate::server::handlers::manager::subnet::SubnetManagerPool;
 use crate::server::handlers::manager::{check_subnet, parse_from};
-use crate::server::JsonRPCRequestHandler;
+use crate::server::{handlers, JsonRPCRequestHandler};
 use anyhow::anyhow;
 use async_trait::async_trait;
 use fvm_shared::address::Address;
-use fvm_shared::econ::TokenAmount;
 use ipc_sdk::subnet_id::SubnetID;
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
@@ -47,11 +46,7 @@ impl JsonRPCRequestHandler for SendValueHandler {
             Some(conn) => conn,
         };
 
-        let amount = f64_to_token_amount(request.amount);
-        if !amount.is_positive() {
-            return Err(anyhow!("invalid amount to send: {:}", request.amount));
-        }
-
+        let amount = handlers::f64_to_token_amount(request.amount)?;
         let subnet_config = conn.subnet();
         check_subnet(subnet_config)?;
 
@@ -63,23 +58,5 @@ impl JsonRPCRequestHandler for SendValueHandler {
         conn.manager().send_value(from, to, amount).await?;
 
         Ok(())
-    }
-}
-
-fn f64_to_token_amount(f: f64) -> TokenAmount {
-    let precision = TokenAmount::PRECISION as f64;
-    // no rounding, just the integer part
-    TokenAmount::from_atto(f64::trunc(f * precision) as u64)
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::server::send_value::f64_to_token_amount;
-    use fvm_shared::econ::TokenAmount;
-
-    #[test]
-    fn test_amount() {
-        let amount = f64_to_token_amount(1.2f64);
-        assert_eq!(amount, TokenAmount::from_atto(1200000000000000000u64));
     }
 }
