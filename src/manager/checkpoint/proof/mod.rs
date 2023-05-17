7d6dae222258ef3f29d95680f98d14f9083cfeb8
cryptoAtwill <willes.lau@protocol.ai>
2023-05-17 17:06:13 +0800

    update comment

--- src/manager/checkpoint/proof/mod.rs
+++ src/manager/checkpoint/proof/mod.rs
@@ -9,7 +9,7 @@ pub use crate::manager::checkpoint::proof::v1::V1Proof;
 use fvm_shared::clock::ChainEpoch;
 use serde::{Deserialize, Serialize};
 
-/// The different version of checkpoint proofs
+/// The different versions of checkpoint proof
 #[derive(Serialize, Deserialize, Debug)]
 pub enum CheckpointProof {
     V1(V1Proof),
